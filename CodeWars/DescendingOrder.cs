6289d37cfa1c5f69b51ae903b29fa51c0827a38c
Lietrix <36711347+Lietrix@users.noreply.github.com>
2019-02-14 10:41:31 -0800

    Added new programs

---
+++ CodeWars/DescendingOrder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    class Kata
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DescendingOrder(56856));
+        }
+
+        public static int[] ConvertInt(int input)   // --- CONVERTS AN INT INTO AN ARRAY --
+        {
+            int[] output = new int[input.ToString().Length];
+            int i = 0;
+            while (input > 0) 
+            {
+                output[i] = input % 10;
+                input /= 10;
+                i++;
+            }
+            return output;
+        }
+
+
+        public static int DescendingOrder(int num)
+        {
+            int [] num1 = ConvertInt(num);
+            for (int i = 0; i < num.ToString().Length - 1; i++)
+            {
+                for (int j = 0; j < num.ToString().Length - 1; j++)
+                {   
+                    if(num1[j] < num1[j + 1])
+                    {
+
+                    int temp = num1[j];
+                    num1[j] = num1[j + 1];
+                    num1[j + 1] = temp;
+
+                    }
+                }
+            }
+
+            string result = String.Empty;
+            for (int i = 0; i < num1.Length; i++)
+            {
+                result += num1[i];
+            }
+
+            return Int32.Parse(result);
+   
+
+        }
+    }
+}
+    
+
+
---
+++ Sorts/Sorts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    class Sorts
+    {
+        public static void BubbleSort(int[] num)
+        {
+            for (int i = 0; i < num.ToString().Length - 1; i++)
+            {
+                for (int j = 0; j < num.ToString().Length - 1; j++)
+                {
+                    if (num[j] < num[j + 1])
+                    {
+                        int temp = num[j];
+                        num[j] = num[j + 1];
+                        num[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
