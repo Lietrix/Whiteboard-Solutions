8a8c0d05106c15540bc67142cd90d9d92cf2ba94
Lietrix <36711347+Lietrix@users.noreply.github.com>
2019-02-26 14:16:37 -0800

    Organize

---
+++ CodeWars/ReverseArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    class ReverseArray
+    {
+        static int[] Reverse(int[] input)
+        {
+            int[] output = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[input.Length - (i + 1)] = input[i];
+            }
+
+            foreach (var x in output)
+            {
+                Console.WriteLine(x);
+            }
+            return output;
+        }
+    }
+}
