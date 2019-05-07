552ea257f5a5420936ddd8261e8aaa160d39e500
Lietrix <joshdot@gmail.com>
2019-05-07 09:50:16 -0700

    Codewars, inner strings sorted in descending order while keeping first and last characters

---
+++ CodeWars/ConsecutiveSums.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    //You are given a list/array which contains only integers (positive and negative).
+    //Your job is to sum only the numbers that are the same and consecutive. 
+    //The result should be one list.
+
+    // example: [1,4,4,4,0,4,3,3,1] should return [1,12,0,4,6,1]
+
+    class ConsecutiveSums
+    {
+        public static List<int> SumConsecutives(List<int> s)
+        {
+            List<int> result = new List<int>();
+            int y = 0;
+            foreach (int x in s)
+            {
+                if (y != 0)
+                {
+                    y = x;
+                }
+
+                if (x == s.IndexOf(x) - 1)
+                {
+                    y += x;
+                }
+
+                else
+                {
+                    result.Add(y);
+                    y = 0;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CodeWars/SortingInner.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Linq;
+
+using System.Collections.Generic;
+
+
+namespace CodeWars
+{
+    class SortingInner
+    {
+        public static string SortTheInnerContent(string words)
+        {
+            string result = string.Empty;
+            var sep = words.Split(' ');
+
+            char temp1, temp2;
+
+            foreach(var x in sep)
+            {
+                if(x.Length <= 2)
+                {
+                    result += x;
+                    result += ' ';
+                }
+                else
+                {
+                    List<char> inner = x.ToList();
+
+                    temp1 = inner.First();
+                    temp2 = inner.Last();
+                    inner.RemoveAt(0);
+                    inner.RemoveAt(inner.Count - 1);
+                    inner.Sort();
+                    inner.Reverse();
+                    string Inner = string.Empty;
+                    foreach(var p in inner)
+                    {
+                        Inner += p;
+                    }
+                
+                    result += $"{temp1}{Inner}{temp2} "; 
+                }
+            }
+            return result.TrimEnd();
+        }
+    }
+}
